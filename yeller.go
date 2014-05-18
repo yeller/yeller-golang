ce7246470d9bb8b2b25155b88b917c1f188696b2
Leo Cassarani <leo.cassarani@me.com>
2014-05-18 20:49:03 +0000

    Add stack traces to the exception notification payload

--- yeller.go
+++ yeller.go
@@ -5,12 +5,15 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"runtime"
+	"strconv"
+	"strings"
 )
 
 type ErrorNotification struct {
 	Type          string                 `json:"type"`
 	Message       string                 `json:"message"`
-	StackTrace    []string               `json:"stacktrace"`
+	StackTrace    []StackFrame           `json:"stacktrace"`
 	Url           string                 `json:"url"`
 	Host          string                 `json:"host"`
 	CustomData    map[string]interface{} `json:"custom-data"`
@@ -18,7 +21,16 @@ type ErrorNotification struct {
 	ClientVersion string                 `json:"client-version"`
 }
 
-const CLIENT_VERSION = "yeller-golang: 0.0.1"
+type StackFrame struct {
+	Filename     string
+	LineNumber   string
+	FunctionName string
+}
+
+const (
+	CLIENT_VERSION  = "yeller-golang: 0.0.1"
+	MAX_STACK_DEPTH = 256
+)
 
 var apiKey string
 
@@ -27,7 +39,7 @@ func Start(newApiKey string) {
 }
 
 func Notify(err error) {
-	notification := NewErrorNotification(err)
+	notification := newErrorNotification(err)
 
 	json, err := json.Marshal(notification)
 	if err != nil {
@@ -36,19 +48,53 @@ func Notify(err error) {
 	}
 
 	url := "https://collector1.yellerapp.com/" + apiKey
-	resp, err := http.Post(url, "application/json", bytes.NewReader(json))
+	_, err = http.Post(url, "application/json", bytes.NewReader(json))
 	if err != nil {
 		log.Println(err)
 		return
 	}
-
-	log.Println(resp)
 }
 
-func NewErrorNotification(err error) *ErrorNotification {
+func (f StackFrame)MarshalJSON() ([]byte, error) {
+	fields := []string{f.Filename, f.LineNumber, f.FunctionName}
+	return json.Marshal(fields)
+}
+
+func newErrorNotification(err error) *ErrorNotification {
 	return &ErrorNotification{
-		Type: "error",
-		Message: err.Error(),
+		Type:          "error",
+		Message:       err.Error(),
+		StackTrace:    applicationStackTrace(),
 		ClientVersion: CLIENT_VERSION,
 	}
+}
+
+func applicationStackTrace() (stackTrace []StackFrame) {
+	for i := 1; i <= MAX_STACK_DEPTH + 1; i++ {
+		pc, file, line, ok := runtime.Caller(i)
+		if !ok {
+			break
+		}
+
+		// Ignore all stack frames coming from this package
+		if strings.Contains(file, "github.com/yeller/yeller-golang") {
+			continue
+		}
+
+		frame := StackFrame{
+			Filename:     file,
+			LineNumber:   strconv.Itoa(line),
+			FunctionName: functionName(pc),
+		}
+		stackTrace = append(stackTrace, frame)
+	}
+	return stackTrace
+}
+
+func functionName(pc uintptr) string {
+	fn := runtime.FuncForPC(pc)
+	if fn == nil {
+		return "???"
+	}
+	return fn.Name()
 }
