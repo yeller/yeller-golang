b68a73d97b82ec76c6662e738a9d5c222a86de80
Leo Cassarani <leo.cassarani@me.com>
2014-05-18 21:17:30 +0000

    Make hostname fields of yeller.Client private

--- client.go
+++ client.go
@@ -7,14 +7,14 @@ import (
 
 type Client struct {
 	ApiKey          string
-	LastHostnameIdx int
-	Hostnames       []string
 	Version         string
+	lastHostnameIdx int
+	hostnames       []string
 }
 
 const CLIENT_VERSION = "yeller-golang: 0.0.1"
 
-func NewClient(apiKey string) *Client {
+func NewClient(apiKey string) (client *Client) {
 	rand.Seed(time.Now().UTC().UnixNano())
 
 	hostnames := []string{
@@ -27,17 +27,17 @@ func NewClient(apiKey string) *Client {
 
 	return &Client{
 		ApiKey:          apiKey,
-		LastHostnameIdx: rand.Intn(len(hostnames)),
-		Hostnames:       hostnames,
 		Version:         CLIENT_VERSION,
+		lastHostnameIdx: rand.Intn(len(hostnames)),
+		hostnames:       hostnames,
 	}
 }
 
 func (c *Client) Hostname() string {
-	return c.Hostnames[c.LastHostnameIdx]
+	return c.hostnames[c.lastHostnameIdx]
 }
 
 func (c *Client) NextHostname() string {
-	c.LastHostnameIdx++
+	c.lastHostnameIdx++
 	return c.Hostname()
 }
